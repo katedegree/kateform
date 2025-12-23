de67de97004d44225b2b223b202cb40ec010083f
Kate0418 <nakao.keito0418@gmail.com>
2025-12-23 13:19:34 +0900

    チェックボックス修正

--- src/internal/components/icons/select-check-icon.tsx
+++ src/internal/components/icons/select-check-icon.tsx
@@ -2,15 +2,14 @@ export function SelectCheckIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="20"
+      height="20"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="w-[1lh] h-lh"
     >
       <path d="M20 6 9 17l-5-5" />
     </svg>
