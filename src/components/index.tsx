b2c24e2c25724dcfe4529024abaa8c1d43bc3372
Kate0418 <nakao.keito0418@gmail.com>
2026-01-27 15:02:33 +0900

    微修正

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -11,6 +11,7 @@ export * from "./input/password-input/password-input";
 export * from "./input/select-input/multi-select-input/multi-select-input";
 export * from "./input/select-input/select-input/select-input";
 export * from "./input/text-input/text-input";
+export * from "./input/textarea-input/textarea-input";
 
 // provider系
 export * from "./provider/kateform-provider/kateform-provider";
